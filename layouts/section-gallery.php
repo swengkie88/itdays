13c92021b4b5bf6157c533b0a33d3fddfec8bb52
nilham92 <42111838+nilham92@users.noreply.github.com>
2018-10-05 16:22:34 -0700

    Add files via upload

---
+++ layouts/section-gallery.php
@@ -0,0 +1,75 @@
+<body style="background: #373C53">
+    <div class="banner" style="background: url('assets/img/icons/pattern.png'); background-size: 400px; background-position: center;">
+        <div class="overlay-filter">
+            <div class="wrapper flexContainer">
+                <div class="jumbotron">
+                    <div class="avatar">
+                        <img src="assets/img/icons/camera-shutter.png" alt="" style="margin-bottom: 30px;">
+                    </div>
+                    <h1>Galeri</h1>
+                    <h3 style="margin-top: 30px;">Dokumentasi Acara IT DAYS dari tahun ke tahun</h3>
+                    <button class="btn" style="margin-top: 30px;">Tambah Foto</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="wrapper" style="width: 80%">
+        <div class="galleryWrapper">
+            <div class="image">
+                <img id="imageList" src="assets/img/gallery/gambar-1.jpg">
+                <!-- <div class="overlayImgGallery"></div> -->
+            </div>
+            <div class="image">
+                <img id="imageList" src="assets/img/gallery/gambar-2.jpg">
+                <!-- <div class="overlayImgGallery"></div> -->
+            </div>
+            <div class="image">
+                <img src="assets/img/gallery/gambar-3.jpg">
+                <!-- <div class="overlayImgGallery"></div> -->
+            </div>
+            <div class="image">
+                <img src="assets/img/gallery/gambar-4.jpg">
+                <!-- <div class="overlayImgGallery"></div> -->
+            </div>
+            <div class="image">
+                <img src="assets/img/gallery/gambar-5.jpg">
+                <!-- <div class="overlayImgGallery"></div> -->
+            </div>
+            <div class="image">
+                <img src="assets/img/gallery/gambar-6.jpg">
+                <!-- <div class="overlayImgGallery"></div> -->
+            </div>
+            <div class="image">
+                <img src="assets/img/gallery/gambar-7.jpg">
+                <!-- <div class="overlayImgGallery"></div> -->
+            </div>
+            <div class="image">
+                <img src="assets/img/gallery/gambar-8.jpg">
+                <!-- <div class="overlayImgGallery"></div> -->
+            </div>
+            <div class="image">
+                <img src="assets/img/gallery/gambar-9.jpg">
+                <!-- <div class="overlayImgGallery"></div> -->
+            </div>
+            <div class="image">
+                <img src="assets/img/gallery/gambar-10.jpg">
+                <!-- <div class="overlayImgGallery"></div> -->
+            </div>
+            <div class="image">
+                <img src="assets/img/gallery/gambar-11.jpg">
+                <!-- <div class="overlayImgGallery"></div> -->
+            </div>
+            <div class="image">
+                <img src="assets/img/gallery/gambar-12.jpg">
+                <!-- <div class="overlayImgGallery"></div> -->
+            </div>
+            <div class="image">
+                <img src="assets/img/gallery/gambar-13.jpg">
+                <!-- <div class="overlayImgGallery"></div> -->
+            </div>
+            <div class="image">
+                <img src="assets/img/gallery/gambar-14.jpg">
+                <!-- <div class="overlayImgGallery"></div> -->
+            </div>
+        </div>
+    </div>
